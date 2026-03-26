98f6d6cd04df7130f422781541147f7a598b408e
Sumiya Barman <sumiyabarman573@gmail.com>
2026-03-26 21:04:59 +0530

    third

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['logo192.png', 'logo512.png'],
       manifest: {
         name: 'My App',
         short_name: 'App',
@@ -19,12 +19,12 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: '/logo192.png',
+            src: 'logo192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/logo512.png',
+            src: 'logo512.png',
             sizes: '512x512',
             type: 'image/png'
           }
